16ea4faac566d2b13c3c9d099378dfbad74661f0
Montrell <mjubilee@leaptodigital.com>
2024-07-22 21:04:27 -0400

    update portfolio project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -163,6 +163,30 @@ const Home = () => {
                 </div>
               </div>
             </div>
+            <div className="group relative overflow-hidden rounded-lg shadow-lg flex flex-col h-full transition-transform duration-300 hover:scale-105">
+              <div className="relative h-60">
+                <img
+                  src="/demo-calculator.png"
+                  alt="Project 3"
+                  className="absolute inset-0 w-full h-full object-cover"
+                />
+              </div>
+              <div className="flex flex-col justify-between p-6 bg-gray-800 text-white h-full">
+              <p className="text-lg">
+                Developed using .NET C# and MAUI for cross-platform applications, creating seamless and efficient mobile solutions.
+              </p>
+                <div className="mt-4 flex flex-col items-center gap-2">
+                  <a
+                    href="https://github.com/mjubilee1/demo-calculator"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-flex items-center justify-center rounded-md bg-blue-600 px-6 py-2 text-sm font-medium text-white shadow-lg transition-colors hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  >
+                    Source Code
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
